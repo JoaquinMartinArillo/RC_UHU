3a3ea418fce8b4a24b3fc852b43d04f525b6be9b
JoaquinMartinArillo <joaquin.martin1@alu.uhu.es>
2025-03-20 09:56:10 +0100

    Update Listas 20 marzo.pl

--- Apuntes Prolog/Listas.pl
+++ Apuntes Prolog/Listas.pl
@@ -30,9 +30,35 @@ elemento_n([Cab|_], 1, Cab).
 elemento_n([Cab|Resto], N, R):-
 	N > 1,
 	N2 is N-1,
-	elemento_n(Resto, N2, R).
+	elemento_n(Resto, N2, R). 
 	
 /*
 Comprime(Lista, R). Es cierto si R unifica con una lista de la siguiente forma:
 	Comprime([a,a,a,b,b,c,a], R). es R=[(a,3),(b,2),(c,1),(a,1)].
 */
+
+
+/*
+my_reverse(?Lista, ?Resultado) es cierto si resultado unifica con una lista que contiene  los elementos
+	de lista en orden inverso
+ my_reverse([1,2,3], R).
+ R = [3,2,1]
+
+ Si pones ? en los parametros significa que puede ser una variable libre o instanciada
+
+ *USO DE append/3 -> append ([1,2,3], [4,5], R). -> R = [1,2,3,4,5]. es decir, concatena 2 listas
+*/
+
+my_reverse([], []).
+my_reverse([Cabeza|Resto], R2):- my_reverse(Resto, R), // Si fuese cierto en R tendriamos Resto en orden inverso
+	append(R, [Cabeza], R2).
+
+/*
+my_append(?Lista1, ?Lista2, ?Resultado)
+	es cierto si unifica con la concatenacion de las 2 listas
+*/
+
+/* Probemos construyendo la induccion con la primera variable (la 2º siempre igual) */
+my_append([], List2, List2).
+my_append([Cab|Resto], List2, [Cab|R]):-
+	my_append(Resto, List2, R). //Si fuese verdad en R tenemos concatenado Resto y List2
