0978bff0d8103c1c379ee9fa964ff34afcf8b99e
JoaquinMartinArillo <joaquin.martin1@alu.uhu.es>
2025-04-10 09:41:19 +0200

    Update Burbuja.pl

--- Ejercicios/Burbuja.pl
+++ Ejercicios/Burbuja.pl
@@ -23,13 +23,15 @@ ordenada([Cab1, Cab2|Resto]) :-
 	Cab1<Cab2, 
 	ordenada([Cab2|Resto]).
 
-%Caso base
+%Caso base - Lista ya ordenada
 burbuja(Lista, Lista):- ordenada(Lista).
+
 %Caso de una lista desordenada
 burbuja(Lista, R):- 
 	append(L1, [E1,E2|L2], Lista), E1 > E2,
 	append(L1, [E2,E1|L2], Lista2),
 	burbuja(Lista2, R).
+ 
 %Caso de una lista ya ordenada
 burbuja(Lista, R):- 
 	append(L1, [E1,E2|L2], Lista), E1 =< E2,	
