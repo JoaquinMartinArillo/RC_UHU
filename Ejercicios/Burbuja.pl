8f870475d6bf7a0cc506894454fade0fb013883d
JoaquinMartinArillo <joaquin.martin1@alu.uhu.es>
2025-04-10 09:03:16 +0200

    Update and rename Ejercicios/Propuestos/Burbuja.pl to Ejercicios/Burbuja.pl

---
+++ Ejercicios/Burbuja.pl
@@ -0,0 +1,21 @@
+/* Ordenacion por burbuja
+ordena_burbuja(+Lista, -ListaR).
+Es cierto cuando ListaR unifica con una lista que contiene los mismos elementos que 
+Lista ordenados de menor a mayor.
+
+En este caso la induccion se centrara en el numero de movimientos que quedan para tener
+la lista ordenada.
+
+*/
+
+/*
+ordenada(Lista) es cierto si los elementos estan ordenados de menor a mayor
+No estamos ordenando sino viendo si la lista ya esta ordenada
+*/
+
+%Lista vacia
+ordenada([]).
+%Lista de un elemento
+ordenado([_]).
+%Lista de 2 o mas elementos
+ordenada([Cab1, Cab2|Resto]):- Cab1 < Cab2, ordenada([Cab2|Resto]).
--- Ejercicios/Propuestos/Burbuja.pl
+++
@@ -1,6 +0,0 @@
-/* Ordenacion por burbuja
-ordena_burbuja(+Lista, -ListaR).
-Es cierto cuando ListaR unifica con una lista que contiene los mismos elementos que 
-Lista ordenados de menor a mayor
-*/
-
