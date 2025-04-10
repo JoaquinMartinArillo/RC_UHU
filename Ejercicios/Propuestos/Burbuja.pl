9e0a42e856a788408f52fb8233ecc8a1e026be58
JoaquinMartinArillo <joaquin.martin1@alu.uhu.es>
2025-04-10 08:45:36 +0200

    Create Burbuja.pl

---
+++ Ejercicios/Propuestos/Burbuja.pl
@@ -0,0 +1,6 @@
+/* Ordenacion por burbuja
+ordena_burbuja(+Lista, -ListaR).
+Es cierto cuando ListaR unifica con una lista que contiene los mismos elementos que 
+Lista ordenados de menor a mayor
+*/
+
