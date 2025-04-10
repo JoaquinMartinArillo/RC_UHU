2d7f4c99b464ef51e4a2a42539905d714ebe08d8
JoaquinMartinArillo <joaquin.martin1@alu.uhu.es>
2025-04-10 13:01:01 +0200

    Create ZebraPuzzle.pl

---
+++ Exercism/ZebraPuzzle.pl
@@ -0,0 +1,38 @@
+/*Tenemos 5 casas con:
+-Persona (nacionalidad)
+-Color
+-Bebida
+-Hobby
+-Mascota
+
+There are five houses.
+The Englishman lives in the red house.
+The Spaniard owns the dog.
+The person in the green house drinks coffee.
+The Ukrainian drinks tea.
+The green house is immediately to the right of the ivory house.
+The snail owner likes to go dancing.
+The person in the yellow house is a painter.
+The person in the middle house drinks milk.
+The Norwegian lives in the first house.
+The person who enjoys reading lives in the house next to the person with the fox.
+The painters house is next to the house with the horse.
+The person who plays football drinks orange juice.
+The Japanese person plays chess.
+The Norwegian lives next to the blue house.
+*/
+
+:- use_module(library(clpfd)).
+
+
+
+
+
+
+
+
+
+
+
+
+
