2b7e78bd22f52362191ff3e786e0379131dfd1fd
JoaquinMartinArillo <joaquin.martin1@alu.uhu.es>
2025-04-10 09:37:18 +0200

    Update Burbuja.pl

--- Ejercicios/Burbuja.pl
+++ Ejercicios/Burbuja.pl
@@ -6,11 +6,12 @@ Lista ordenados de menor a mayor.
 En este caso la induccion se centrara en el numero de movimientos que quedan para tener
 la lista ordenada.
 
-*/
-
-/*
 ordenada(Lista) es cierto si los elementos estan ordenados de menor a mayor
-No estamos ordenando sino viendo si la lista ya esta ordenada
+No estamos ordenando, sino viendo si la lista ya esta ordenada.
+
+TRUCO: Obtener todos los pares consecutivos de una lista
+append(_, [E1,E2|_), [8,4,3,1,7,2,5,6]).
+
 */
 
 %Lista vacia
@@ -21,3 +22,30 @@ ordenada([_]).
 ordenada([Cab1, Cab2|Resto]) :- 
 	Cab1<Cab2, 
 	ordenada([Cab2|Resto]).
+
+%Caso base
+burbuja(Lista, Lista):- ordenada(Lista).
+%Caso de una lista desordenada
+burbuja(Lista, R):- 
+	append(L1, [E1,E2|L2], Lista), E1 > E2,
+	append(L1, [E2,E1|L2], Lista2),
+	burbuja(Lista2, R).
+%Caso de una lista ya ordenada
+burbuja(Lista, R):- 
+	append(L1, [E1,E2|L2], Lista), E1 =< E2,	
+	burbuja(Lista2, R).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
