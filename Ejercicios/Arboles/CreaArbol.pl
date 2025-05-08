cd4c1db12863b2d62944c53771fd702731334629
JoaquinMartinArillo <joaquin.martin1@alu.uhu.es>
2025-05-08 09:50:25 +0200

    Update CreaArbol.pl

--- Ejercicios/Arboles/CreaArbol.pl
+++ Ejercicios/Arboles/CreaArbol.pl
@@ -1,13 +1,21 @@
+% Balanceado1
 arbol1(
-	a(1, a(2, a(4, nil, nil), a(5, nil, nil)), a(3, a(6, nil, nil), a(7, a(8, a(9, nil, nil), nil), nil)))
+	a(1, a(2, a(4, nil, nil), a(5, nil, nil)), a(3, a(6, nil, nil), a(7, a(8, nil, nil), nil)))
 ).
 
+% Balanceado2
 arbol2(
 		a(1, a(2, nil, nil), a(3, nil, nil))
 ).
 
+% Arbol de un nodo
 arbol3(
 	a(2, nil, nil)
+).
+
+% Desbalanceado
+arbol4(
+	a(1, a(2, a(4, nil, nil), a(5, nil, nil)), a(3, a(6, nil, nil), a(7, a(8, a(9, nil, nil), nil), nil)))
 ).
 
 /*
@@ -43,7 +51,9 @@ balanceado(a(_,AI,AD)):-
 	altura(AI, A1),
 	altura(AD, A2),
 	Dif is abs(A1 - A2),
-	Dif =< 1.
+	Dif =< 1,
+	balanceado(AI),
+	balanceado(AD).
 
 altura(nil, 0).
 
